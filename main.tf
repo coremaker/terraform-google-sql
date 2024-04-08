abc3c1409355b687eb43234109c175ef8f3af4e9
Cosmin Briscaru <cosmin@coremaker.io>
2024-04-08 12:15:02 +0100

    add deletion_protection_enabled

--- main.tf
+++ main.tf
@@ -17,9 +17,10 @@ resource "google_sql_database_instance" "main" {
   }
 
   settings {
-    tier              = var.machine_type
-    disk_size         = var.disk_size
-    availability_type = var.availability_type
+    tier                        = var.machine_type
+    disk_size                   = var.disk_size
+    availability_type           = var.availability_type
+    deletion_protection_enabled = var.deletion_protection
 
     ip_configuration {
       dynamic "authorized_networks" {
